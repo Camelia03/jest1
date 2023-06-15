2cacaa8ce26ca35ec190496b12cacde0c275ccc1
Camelia03 <camelia_weber@yahoo.com>
2023-06-15 18:46:53 +0000

    Add lightsOn function

--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,6 @@
 let game = {
     score: 0,
-    currentGame: 0,
+    currentGame: [],
     playerMoves: [],
     choices: ["button1", "button2", "button3", "button4"],
 }
@@ -23,4 +23,11 @@ function showScore() {
     document.getElementById("score").innerText = game.score;
 }
 
-module.exports = { game, newGame, showScore, addTurn };
+function lightsOn(param1) {
+    document.getElementById(param1).classList.add("light");
+    setTimeout(() => {
+        document.getElementById(param1).classList.remove("light");
+    }, 400);
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn };
